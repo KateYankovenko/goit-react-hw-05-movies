8728769c8adb677826eb671ee328cf28f22b0d14
Kate Yankovenko <k.yankovenko@ukr.net>
2022-08-19 20:28:35 +0200

    added styles

---
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+text-decoration: none;
+display: block;
+color: ${p => p.theme.colors.secondary};
+font-size: ${p => p.theme.fontSizes.l};
+font-weight: ${p => p.theme.fontWeights.bold};
+&:hover,
+&:focus{
+    color: ${p => p.theme.colors.black};
+}
+&:not(:last-child){
+    margin-right: ${p => p.theme.space[7]}px;
+   
+}
+`
